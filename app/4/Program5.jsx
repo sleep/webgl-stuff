68e3d9a2e2c0b223231fa6c75251d879629fd8ac
Sean Lee <freshdried@gmail.com>
2015-10-09 20:22:47 -0400

    added boolean intersection

--- app/4/Program5.jsx
+++ app/4/Program5.jsx
@@ -35,6 +35,7 @@ struct Surface {
   vec3 N; // surface normal (orientation)
 
   vec3 material;
+  float shinyness;
 };
 
 struct Plane {
@@ -42,6 +43,7 @@ struct Plane {
   vec3 N; // normal
 
   vec3 material;
+  float shinyness;
 };
 
 struct Sphere {
@@ -49,10 +51,16 @@ struct Sphere {
   float r; // radius
 
   vec3 material;
+  float shinyness;
+};
+
+struct IntersectionObj{
+  Sphere sphere[6];
 };
 
 struct World {
-  Sphere sphere[3];
+  // Sphere sphere[1];
+  IntersectionObj io[1];
   Plane plane[1];
 
   Light light[3];
@@ -90,11 +98,11 @@ vec3 errorColor() {
 // ray tracing functions:
 
 
-// raySphere :: (vec3 V, vec3 W, vec4 sph) -> float t
+// raySphere :: (vec3 V, vec3 W, vec4 sph) -> vec2 (t_min, t_max)
 // compute value of parameter t
 // in intersection between ray W*t with sphere.
 // Returns t if intersection, else return 10000.
-float raySphere(vec3 V, vec3 W, Sphere sphere) {
+vec2 raySphere(vec3 V, vec3 W, Sphere sphere) {
   // ray from center of sphere to V
   vec3 D = V - sphere.P;
 
@@ -107,17 +115,17 @@ float raySphere(vec3 V, vec3 W, Sphere sphere) {
 
 
   if (b > 0.) {
-    return 10000.; //sphere is behind us
+    return vec2(10000., 10000.); //sphere is behind us
   }
 
   float discriminant = b*b - 4. * a * c;
   if (discriminant < 0.) {
-    return 10000.;
+    return vec2(10000., 10000.); // no solutions
   }
   float t_1 = ((-1. * b) + sqrt(discriminant))/(2. * a);
   float t_2 = ((-1. * b) - sqrt(discriminant))/(2. * a);
 
-  return min(t_1, t_2); // minimum = closer point
+  return vec2(min(t_1, t_2), max(t_1, t_2)); // minimum = closer point
 }
 
 float rayPlane(vec3 V, vec3 W, vec3 P, vec3 N) {
@@ -147,19 +155,19 @@ Surface getSurface(vec3 V, vec3 W, World world) {
   float min_t = 10000.;
 
   // loop through spheres
-  for (int i = 0; i < 3; i++) {
-    float t = raySphere(V, W, world.sphere[i]);
-    if (t < min_t) {
-      min_t = t;
+  // for (int i = 0; i < 3; i++) {
+  //   float t = raySphere(V, W, world.sphere[i]).x;
+  //   if (t < min_t) {
+  //     min_t = t;
 
-      surf.S = V + t*W;
-      surf.N = (surf.S - world.sphere[i].P)/ world.sphere[i].r;
-      surf.material = world.sphere[i].material;
-      surf.exists = true;
-    }
-  }
+  //     surf.S = V + t*W;
+  //     surf.N = (surf.S - world.sphere[i].P)/ world.sphere[i].r;
+  //     surf.material = world.sphere[i].material;
+  //     surf.shinyness = world.sphere[i].shinyness;
+  //     surf.exists = true;
+  //   }
+  // }
   //loop through planes
-
   for (int i = 0; i<1; i++) {
     float t = rayPlane(V, W, world.plane[i].P, world.plane[i].N);
     if (t < min_t) {
@@ -168,7 +176,67 @@ Surface getSurface(vec3 V, vec3 W, World world) {
       surf.S = V + t*W;
       surf.N = world.plane[i].N;
       surf.material = world.plane[i].material;
+      surf.shinyness = world.plane[i].shinyness;
       surf.exists = true;
+    }
+  }
+
+  // loop through IntersectionObjects
+  for (int i = 0; i < 1; i++) {
+    // we want to use the surface with the maximum in time
+    // and the minimum out time
+    float max_in = -10000.; // lower bound
+    float min_out = 10000.; // upper bound
+    Surface _surf;
+
+    bool found = true;
+
+    for (int j = 0; j < 6; j++) {
+      vec2 in_out = raySphere(V, W, world.io[i].sphere[j]);
+      float t_in = in_out[0];
+      float t_out = in_out[1];
+
+      // if intersection
+      if (t_in < 10000.) {
+        // INVARIANT: t_in, t_out are under 10000, aka their true values
+        // surface exists
+
+        if (t_in > max_in) { // time in greater than previous max in
+          max_in = t_in;
+
+          // we have to set _surf here, because we cannot have variable array references.
+
+          _surf.S = V + t_in*W;
+          _surf.N = (_surf.S - world.io[i].sphere[j].P)/ world.io[i].sphere[j].r;
+          _surf.material = world.io[i].sphere[j].material;
+          _surf.shinyness = world.io[i].sphere[j].shinyness;
+          _surf.exists = true;
+        }
+        if (t_out < min_out) { // time out less than previous min out
+          min_out = t_out;
+        }
+      } else {
+        // We break and declare not found, because an intersection requires
+        // the point to exist in all n objects
+        found = false;
+        break;
+      }
+    }
+
+    // max_in must be less than max_out, because it must go in before it goes out
+
+    // // max_in must be less than min_t to be in front of all the other objects
+    // found = found && (max_in < min_out);
+
+    // // max_in must be greater than its lower bound (-10000.)
+    // found = found && (max_in > -10000.);
+
+    // // min_out must be less thanthan its upper bound (10000.)
+    // found = found && (min_out < 10000.);
+
+
+    if (found && (max_in < min_t)) {
+      surf = _surf;
     }
   }
   return surf;
@@ -185,14 +253,14 @@ vec3 shade(Surface surf, Light light) {
   // if (dot(surf.N, vec3(0., 0., 1.)) < 0.) return errorColor();
 
   // ambient
-  vec3 a_rgb = surf.material/ 5.;
+  vec3 a_rgb = surf.material/ 6.;
 
   // diffuse (lambert)
-  vec3 d_rgb = vec3(0.5, 0.5, 0.5);
+  vec3 d_rgb = vec3(0.4, 0.4, 0.4);
 
   // specular (blinn)
   vec3 s_rgb = vec3(0.5, 0.5, 0.5); // specular light color
-  float shinyness = 6.;
+  float shinyness = surf.shinyness;
   vec3 eye = -1. * normalize(surf.S);
   vec3 halfway = normalize(-1. * light.dir + eye);
 
@@ -228,7 +296,7 @@ vec3 shadeWithShadows(Surface surf, Light light, World world) {
   Surface dest = getSurface(V_prime, W_prime, world);
 
   if (dest.exists) {
-    vec3 a_rgb = surf.material/ 5.;
+    vec3 a_rgb = surf.material/ 10.;
     return a_rgb;
   }
 
@@ -251,7 +319,7 @@ vec3 rayTrace(vec3 V, vec3 W, World world) {
   Surface surf = getSurface(V, W, world);
 
   if (!surf.exists) {
-    return vec3(0., 0., 0.);
+    return vec3(0.1, 0.2, 0.7);
   }
 
   // SHADE POINT ON IMAGE PLANE
@@ -289,10 +357,10 @@ void main(void) {
   // INITIALIZE LIGHTS
 
   // Light from infinity (diffuse lighting)
-  world.light[0].rgb = vec3(0.5, 0.5, 0.5);
+  world.light[0].rgb = vec3(0.3, 0.3, 0.3);
   world.light[0].dir = -1. * normalize(inverseMercator(uCursor.x, uCursor.y, 1.));
 
-  world.light[1].rgb = vec3(0.5, 0.5, 0.5);
+  world.light[1].rgb = vec3(0.3, 0.3, 0.3);
   world.light[1].dir = normalize(vec3(-1.0, -1.0, -1.0));
 
   world.light[2].rgb = vec3(0.0, 0.0, 0.0);
@@ -302,35 +370,61 @@ void main(void) {
 
 
   // INITIALIZE OBJECTS
-  // (orbiting spheres);
 
-  // sphere dynamics
-  vec3 center = vec3(0., 0., -1.);
-  float r_1 = 0.5; float theta_1 = uTime/8.;
-  float r_2 = 0.08; float theta_2 = uTime * 2.;
 
-  // rotate around y axis
-  vec3 orbit_1 = vec3(r_1 * cos(theta_1),0,  r_1 * sin(theta_1));
-  vec3 orbit_2 = vec3(r_2 * cos(theta_2),0,  r_2 * sin(theta_2));
 
-  world.sphere[0].r = 0.25;
-  world.sphere[1].r = 0.05;
-  world.sphere[2].r = 0.01;
+  float r_1 = 0.25; //radius of spheres
+  float r_2 =  0.09; //distance from center
 
-  world.sphere[0].P = vec3(center);
-  world.sphere[1].P = vec3(world.sphere[0].P + orbit_1);
-  world.sphere[2].P = vec3(world.sphere[1].P + orbit_2);
+  float maxheight = 0.1;
+  float height =  maxheight + sin(uTime) * maxheight;// in multiples of ball heights
+  vec3 center = vec3(0., height, -1.);
+  // r_2  must be less than r_1
 
-  // sphere properties
-  world.sphere[0].material = vec3(1.0, 1.0, 1.0);
-  world.sphere[1].material = vec3(1.0, 1.0, 1.0);
-  world.sphere[2].material = vec3(1.0, 1.0, 1.0);
+  float theta = uTime/3.;
+  float k = 1.0;
+  float phase0 = sin(theta * 2. + 0. * PI) * k;
+  float phase1 = sin(theta * 2. + 1. * PI) * k;
+  float phase2 = sin(theta * 3. + 0. * PI) * k;
+  float phase3 = sin(theta * 3. + 1. * PI) * k;
+  float phase4 = sin(theta * 5. + 0. * PI) * k;
+  float phase5 = sin(theta * 5. + 1. * PI) * k;
 
+  world.io[0].sphere[0].r = r_1 + 0.01* phase0;
+  world.io[0].sphere[1].r = r_1 + 0.01* phase1;
+  world.io[0].sphere[2].r = r_1 + 0.01* phase2;
+  world.io[0].sphere[3].r = r_1 + 0.01* phase3;
+  world.io[0].sphere[4].r = r_1 + 0.01* phase4;
+  world.io[0].sphere[5].r = r_1 + 0.01* phase5;
+
+
+  float phi = sin(uTime) * 6.;
+
+  vec3 dirA = normalize(vec3(cos(phi), 0., sin(phi)));
+  vec3 dirB = normalize(vec3(0., 1., 0.));
+  vec3 dirC = normalize(vec3(-1. * sin(phi),0.,  cos(phi)));
+
+
+  world.io[0].sphere[0].P = center +  1.0 * r_2 * dirA;
+  world.io[0].sphere[1].P = center + -1.0 * r_2 * dirA;
+  world.io[0].sphere[2].P = center +  1.0 * r_2 * dirB;
+  world.io[0].sphere[3].P = center + -1.0 * r_2 * dirB;
+  world.io[0].sphere[4].P = center +  1.0 * r_2 * dirC;
+  world.io[0].sphere[5].P = center + -1.0 * r_2 * dirC;
+
+  for (int i = 0 ; i<6; i++) {
+    world.io[0].sphere[i].material = vec3(1., 1., 1.);
+  }
+
+  for (int i = 0; i < 6; i++) {
+    world.io[0].sphere[i].shinyness = 5.;
+  }
 
   //plane
   world.plane[0].N = vec3(0., 1., 0.);
   world.plane[0].P = vec3(0., -0.25, 0.);
-  world.plane[0].material = vec3(0.1, 0.1, 0.1);
+  world.plane[0].material = vec3(0.0, 1.0, 0.0);
+  world.plane[0].shinyness = 3.;
 
   // RAY TRACE
 
