f7bb512e55b675f26035283aa04115b3a5fc0801
Sean Lee <freshdried@gmail.com>
2015-10-11 17:29:20 -0400

    implemented reflection

--- app/4/index.jsx
+++ app/4/index.jsx
@@ -11,28 +11,33 @@ export default React.createClass({
 
 \`(ambient) + (diffuse Lambert reflectance) + (Phong specular reflectance)\`
 
-`,
+         `,
       React.createElement(require("./Program1.jsx")),
       `
 ## Blinn-Phong model for specular reflectance:
 
 \`(ambient) + (diffuse Lambert reflectance) + (Blinn-Phong specular reflectance)\`
 
-`,
+         `,
       React.createElement(require("./Program2.jsx")),
       `
 ## Shadows
-`,
+        `,
       React.createElement(require("./Program3.jsx")),
       `
 ## Add a plane
-`,
+        `,
       React.createElement(require("./Program4.jsx")),
       `
 ## Boolean intersection
 
-`,
+         `,
       React.createElement(require("./Program5.jsx")),
+      `
+## Reflection
+
+`,
+      React.createElement(require("./Program6.jsx")),
     ];
     return (
       <SuperLiterate>
